b4fde49a17f3da0c00b3d2f6e8281da8996ebf9b
HeshamFawzy <heshamfawzy2772@gmail.com>
2019-06-11 20:34:12 +0200

    toastr

--- Consumer/lib/js/CustomerController.js
+++ Consumer/lib/js/CustomerController.js
@@ -9,25 +9,65 @@ angular.module('apiApp').controller('CustomerController', function($scope, $http
     });
 
     $scope.GetCustomers = function (data){
-        $scope.Customers = CustomersService.query();
+        $scope.Customers = CustomersService.query(function(response) {
+            if(response.$promise.$$state.status == 0){
+                toastr.warning("Warning");
+            } else if(response.$promise.$$state.status == 1) {
+                toastr.success("Get Data Successfully");
+            } else {
+                toastr.error("Error While Getting Data");
+            } 
+        });
     }
 
     $scope.GetCustomerById = function(){
         if($stateParams.id != undefined){
-            $scope.Customer = CustomersService.getById({id : $stateParams.id});
+            $scope.Customer = CustomersService.getById({id : $stateParams.id}, function(response){
+                if(response.$promise.$$state.status == 0){
+                    toastr.warning("Warning");
+                } else if(response.$promise.$$state.status == 1) {
+                    toastr.success("Get Data Successfully");
+                } else {
+                    toastr.error("Error While Getting Data");
+                }
+            });
         }
     }
 
     $scope.UpdateCustomer = function (Customer){
-        CustomersService.update({id : $stateParams.id},Customer);
+        CustomersService.update({id : $stateParams.id},Customer, function(response){
+            if(response.$promise.$$state.status == 0){
+                toastr.warning("Warning");
+            } else if(response.$promise.$$state.status == 1) {
+                toastr.success("Get Updated Successfully");
+            } else {
+                toastr.error("Error While Updating Data");
+            } 
+        });
     }
 
     $scope.SaveCustomer = function (Customer){
-        CustomersService.save(Customer);
+        CustomersService.save(Customer, function(response){
+            if(response.$promise.$$state.status == 0){
+                toastr.warning("Warning");
+            } else if(response.$promise.$$state.status == 1) {
+                toastr.success("Get Added Successfully");
+            } else {
+                toastr.error("Error While Adding Data");
+            } 
+        });
     }
 
     $scope.DeleteCustomer = function(id){
-        CustomersService.remove(id);
+        CustomersService.remove(id, function(response){
+            if(response.$promise.$$state.status == 0){
+                toastr.warning("Warning");
+            } else if(response.$promise.$$state.status == 1) {
+                toastr.success("Get Deleted Successfully");
+            } else {
+                toastr.error("Error While Deleting Data");
+            } 
+        });
     }
 
 });
