ca9d2d321b428373f534c3fe0d0aa7d2bbc70629
HeshamFawzy <heshamfawzy2772@gmail.com>
2019-06-12 15:58:14 +0200

    popup done

--- Consumer/lib/js/PopUpController.js
+++ Consumer/lib/js/PopUpController.js
@@ -1,7 +1,6 @@
 angular.module('apiApp').controller('PopUpController', function($scope, $uibModal){
 
         $scope.open = function () {
-            console.log('opening pop up');
             var modalInstance = $uibModal.open({
             templateUrl: 'popup.html',
         });
