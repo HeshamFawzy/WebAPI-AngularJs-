8472412e3c3a2afff2d30bd9ea35c4f2f1e67885
HeshamFawzy <heshamfawzy2772@gmail.com>
2019-06-13 13:29:47 +0200

    get customer id in popup

--- Consumer/lib/js/PopUpController.js
+++ Consumer/lib/js/PopUpController.js
@@ -1,9 +1,14 @@
 angular.module('apiApp').controller('PopUpController',['$scope','$uibModal', function($scope, $uibModal){
 
-    $scope.open = function() {
+    $scope.open = function(Id) {
         var modalInstance = $uibModal.open({
             templateUrl: 'popup.html',
             controller: 'PopUpController2',
+            resolve: {
+                Customer_Id: function () {
+                    return Id;
+                }
+            }
         }).result.catch(function(res) {
             console.log(res);
             if (!(res === 'cancel' || res === 'escape key press')) {
--- Consumer/lib/js/PopUpController2.js
+++ Consumer/lib/js/PopUpController2.js
@@ -1,5 +1,7 @@
-angular.module('apiApp').controller('PopUpController2',['$scope', '$uibModalInstance', function ($scope, $uibModalInstance) {
+angular.module('apiApp').controller('PopUpController2',['$scope', '$uibModalInstance', 'Customer_Id', function ($scope, $uibModalInstance, Customer_Id) {
+        $scope.Customer_ID = Customer_Id;
         $scope.close = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
+            $uibModalInstance.dismiss('cancel');
+            console.log(Customer_Id);
+        };
 }]);
